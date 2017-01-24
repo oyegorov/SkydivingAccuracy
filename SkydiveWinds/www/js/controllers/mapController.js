39d24ca93a7ee8d9bb95b4a6620945c8ae98d523
Oleg Yegorov <yegorov.oleg@gmail.com>
2017-01-23 22:53:50 -0500

    Center maps on selected dropzone

--- SkydiveWinds/www/js/controllers/mapController.js
+++ SkydiveWinds/www/js/controllers/mapController.js
@@ -1,19 +1,50 @@
 ﻿angular.module('starter.controllers')
-    .controller('MapController', function ($scope) {
-        navigator.geolocation.getCurrentPosition(
-              function (position) {
-                  var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    .controller('MapController', function ($scope, $state, settingsService) {
+        $scope.initializeMaps = function () {
+            var locationInfo = settingsService.loadLocationInfo();
 
-                  var mapOptions = {
-                      center: latLng,
-                      zoom: 15,
-                      mapTypeId: google.maps.MapTypeId.ROADMAP
-                  };
+            if (locationInfo == null) {
+                $scope.locationPresent = false;
+                $scope.dropzoneName = null;
+                return;
+            }
 
-                  $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-              },
-              function (error) {
-                  getDropzoneFromService(null, $scope.locationInfo.dropzoneSearchPattern);
-              },
-              { enableHighAccuracy: true, timeout: 5000 });
+            $scope.locationPresent = true;
+            if (locationInfo.dropzoneName != null)
+                $scope.locationName = locationInfo.dropzoneName;
+            else
+                $scope.locationName = '(' + locationInfo.latitude + '; ' + locationInfo.longitude + ')';
+
+            var latLng = new google.maps.LatLng(locationInfo.latitude, locationInfo.longitude);
+            if ($scope.map != null) {
+                $scope.map.panTo(latLng);
+                return;
+            }
+
+            var mapOptions = {
+                center: latLng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.SATELLITE,
+                mapTypeControlOptions: {
+                    mapTypeIds: [google.maps.MapTypeId.SATELLITE]
+                },
+                disableDefaultUI: true,
+                mapTypeControl: true,
+                scaleControl: true,
+                zoomControl: true,
+                zoomControlOptions: {
+                    style: google.maps.ZoomControlStyle.LARGE
+                }
+            };
+
+            $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        }
+
+        $scope.$on('$ionicView.beforeEnter', function () {
+            $scope.initializeMaps();
+        });
+
+        $scope.gotoLocationSettings = function () {
+            $state.transitionTo('app.location');
+        }
     });
